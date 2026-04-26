629943e6cf7c4d4d8acc81545af9a338b1825cb1
mrrajatkumarrai-star <mr.rajatkumarrai@gmail.com>
2026-04-26 12:52:43 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
